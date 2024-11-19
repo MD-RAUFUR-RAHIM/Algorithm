5d846c8284efb8a37ffcfe1ffdb626c6e354c75c
Md Raufur Rahim <raufurrahim2000@gmail.com>
2024-11-19 18:24:07 +0600

    Linear Search releted problem

--- InsertionSort/2.1-3 Corman Algo Book page 43/Program.cs
+++ InsertionSort/2.1-3 Corman Algo Book page 43/Program.cs
@@ -14,31 +14,81 @@ namespace _2._1_3_Corman_Algo_Book_page_43
             public void LinearSearch(int[] A, int value)
             {
                 bool key = false;
-                int j = 0;
-                for(int i=0; i<A.Length; i++)
+                int[] j = new int[A.Length];
+                int count = 0;
+                for (int i = 0; i < A.Length; i++)
                 {
-                    if(A[i] == value)
+                    if (A[i] == value)
                     {
                         key = true;
-                        j = i;
+                        j[count] = i;
+                        count++;
+                        // break;
                     }
                 }
+                //if (key)
+                //{
+                //    Console.WriteLine("The value is in " + j + "th index");
+                //    Console.WriteLine("The value has " + count + " times in the array"); 
+                //}
                 if (key)
                 {
-                    Console.WriteLine("The value is in " + j + "th index");
+                    Console.WriteLine("The value is found at the following indices:");
+                    for (int i = 0; i < count; i++)
+                    {
+                        //if (i < count - 1)
+                        //{
+                        //    Console.Write(j[i] + ", ");
+                        //}
+                        //else
+                        //{
+                        //    Console.Write(j[i]);
+                        //}
+
+                        Console.Write(j[i] + (i < count - 1 ? ", " : "")); // Print indices
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("The value appears " + count + " times in the array.");
                 }
-                else 
+                else
                 {
                     Console.WriteLine("The value does not exist");
                 }
-                
+
             }
+            //public void LinearSearch(int[] A, int value)
+            //{
+            //    bool key = false;
+            //    List<int> indices = new List<int>();
+            //    int count = 0;
+
+            //    for (int i = 0; i < A.Length; i++)
+            //    {
+            //        if (A[i] == value)
+            //        {
+            //            key = true;
+            //            indices.Add(i); 
+            //            count++;      
+            //        }
+            //    }
+
+            //    if (key)
+            //    {
+            //        Console.WriteLine("The value is found at the following indices: " + string.Join(", ", indices));
+            //        Console.WriteLine("The value appears " + count + " times in the array.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The value does not exist in the array.");
+            //    }
+            //}
+
         }
         static void Main(string[] args)
         {
-            int[] A = { 5, 4, 1, 3, 2 };
+            int[] A = { 5, 4, 5, 3, 2 };
             Solution s = new Solution();
-            s.LinearSearch(A, 2);
+            s.LinearSearch(A, 5);
         }
     }
 }
