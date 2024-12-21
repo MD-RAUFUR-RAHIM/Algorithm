16df344fbb97de17e36cb28acee7185d731b6ed9
Md Raufur Rahim <raufurrahim2000@gmail.com>
2024-12-22 03:31:09 +0600

    Palindrome Problem Solved using Array and Reversing Array.

---
+++ 9. Palindrome Number/9. Palindrome Number/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Solution s = new Solution();
+Console.WriteLine(s.IsPalindrome(101));
+public class Solution()
+{
+    public bool IsPalindrome(int x)
+    {
+        string x1 = x.ToString();
+        char[] x2 = x1.ToCharArray();
+        string x3 = ReverseString(x2);
+        if (x1 == x3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    public string ReverseString(char[] c) 
+    {
+        Array.Reverse(c);
+        string reversedString = new string(c);
+        return reversedString;
+    }
+}
