348ab74dd776dd9c67f4a31349d34cca32e07428
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-06-02 12:32:23 +0600

    LeetCode problem 136, 26, 66, 88 done

---
+++ ProgrammingFundamentals/26_Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,22 @@
+﻿ int RemoveDuplicates(int[] nums)
+{
+    if (nums.Length == 0)
+        return 0;
+    int i = 0;
+    for(int j= 1;  j<nums.Length; j++)
+    {
+        if (nums[j] != nums[i])
+        {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+
+int[] x = new int[]
+{
+    0, 0, 1, 1, 2, 3, 3
+};
+int k = RemoveDuplicates(x);
+Console.WriteLine(k);
