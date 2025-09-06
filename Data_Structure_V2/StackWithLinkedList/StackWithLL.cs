bc5d8f8e62781fc6522fe1a75f307f12b4b18b35
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-09-07 02:03:44 +0600

    Implemented Stack with Linked List

---
+++ Data_Structure_V2/StackWithLinkedList/StackWithLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackWithLinkedList
+{
+    public class StackWithLL
+    {
+       LinkedList<int> linkedList = new LinkedList<int>();
+       public void Push(int val)
+        {
+            linkedList.AddFirst(val);
+        }
+        public int Pop() {
+            int val = linkedList.First.Value;
+            linkedList.RemoveFirst();
+            return val;
+        }
+        public int Top()
+        {
+            if (linkedList.Count == 0)
+                throw new InvalidOperationException("Stack is empty.");
+            return linkedList.First.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return linkedList.Count == 0;
+        }
+    }
+}
